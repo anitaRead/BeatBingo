7f521261c4432433f950f6063f9dc6afdab89e5a
Anita Readman <areadman91@gmail.com>
2023-03-26 10:22:43 +0100

    Add unit test for parseInput() - to parse the input.txt file for part 2 of the challenge

--- part2/bingoP2.test.js
+++ part2/bingoP2.test.js
@@ -1,11 +1,11 @@
-const getWinningCard = require("./bingoP2");
+const { getWinningCard, parseInput } = require("./bingoP2");
 
 const numbersCalled = [
   7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18,
   20, 8, 19, 3, 26, 1,
 ];
 
-// losing card
+// Losing card
 
 const losingCard = [
   [2, 23, 15, 5, 92],
@@ -34,6 +34,64 @@ const winningCardColumn = [
   [7, 0, 14, 9, 90],
   [21, 24, 19, 13, 37],
 ];
+
+describe("parseInput()", () => {
+  test("returns correct numbersCalled and bingoCard arrays", () => {
+    const input = `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+      22 13 17 11 0
+      8 2 23 4 24
+      21 9 14 16 7
+      6 10 3 18 5
+      1 12 20 15 19
+
+      3 15 0 2 22
+      9 18 13 17 5
+      19 8 7 25 23
+      20 11 10 24 4
+      14 21 16 12 6
+
+      14 21 17 24 4
+      10 16 15 9 19
+      18 8 23 26 20
+      22 11 13 6 5
+      2 0 12 3 7`;
+
+    const expectedOutput = {
+      bingoCards: [
+        [
+          [22, 13, 17, 11, 0],
+          [8, 2, 23, 4, 24],
+          [21, 9, 14, 16, 7],
+          [6, 10, 3, 18, 5],
+          [1, 12, 20, 15, 19],
+        ],
+        [
+          [3, 15, 0, 2, 22],
+          [9, 18, 13, 17, 5],
+          [19, 8, 7, 25, 23],
+          [20, 11, 10, 24, 4],
+          [14, 21, 16, 12, 6],
+        ],
+        [
+          [14, 21, 17, 24, 4],
+          [10, 16, 15, 9, 19],
+          [18, 8, 23, 26, 20],
+          [22, 11, 13, 6, 5],
+          [2, 0, 12, 3, 7],
+        ],
+      ],
+      numbersCalled: [
+        7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22,
+        18, 20, 8, 19, 3, 26, 1,
+      ],
+    };
+
+    let result = parseInput(input);
+
+    expect(result).toEqual(expectedOutput);
+  });
+});
 
 describe("getWinningCard() returns the winning bingo card", () => {
   test("when two losing cards and one winning card are passed in", () => {
