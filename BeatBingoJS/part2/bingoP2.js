176c8d660c776a62bdbb44f3f01b607bb43b69e9
Anita Readman <areadman91@gmail.com>
2023-03-30 13:16:13 +0100

    Refactor solution to improve readability

--- BeatBingoJS/part2/bingoP2.js
+++ BeatBingoJS/part2/bingoP2.js
@@ -17,21 +17,19 @@ const fs = require("fs");
 
 const parseInput = (data) => {
   const rows = data.trim().split("\n");
-  const separationLine = rows.findIndex((row) => row.trim() === "");
+  const separationLine = rows.findIndex((row) => row === "");
 
   // Numbers called array
   const numbersCalled = rows[0]
     .split(",")
     .map((numberString) => parseInt(numberString));
 
-  const cardRows = rows
-    .slice(separationLine + 1)
-    .filter((row) => row.trim() !== "");
+  const cardRows = rows.slice(separationLine + 1).filter((row) => row !== "");
 
   // Bingo cards array
   const bingoCards = [];
   for (let i = 0; i < 3; i++) {
-    const card = cardRows.slice((i * 5), (i * 5 + 5)).map((row) =>
+    const card = cardRows.slice(i * 5, i * 5 + 5).map((row) =>
       row
         .split(" ")
         .filter((str) => str !== "")
@@ -49,33 +47,30 @@ const getWinningCard = (bingoCards, numbersCalled) => {
   let columnLength = 5;
 
   // Loop through all bingo cards
-  for (let i = 0; i < bingoCards.length; i++) {
-    let currentBingoCard = bingoCards[i];
+  for (let bingoCard of bingoCards) {
+    // Check each row
+    for (let i = 0; i < rowLength; i++) {
+      let matchingNumbers = bingoCard[i].filter((number) =>
+        numbersCalled.includes(number)
+      );
 
-    // Loop through current bingo card
-    for (let j = 0; j < currentBingoCard.length; j++) {
+      if (matchingNumbers.length === rowLength) {
+        return bingoCard;
+      }
+    }
+
+    // Check each column
+    for (let i = 0; i < columnLength; i++) {
       let matchingNumbers = [];
 
-      // Check each row
-      matchingNumbers = currentBingoCard[j].filter((number) =>
-        numbersCalled.includes(number)
-      );
-      if (matchingNumbers.length === rowLength) {
-        return currentBingoCard;
+      for (let j = 0; j < rowLength; j++) {
+        if (numbersCalled.includes(bingoCard[j][i])) {
+          matchingNumbers.push(bingoCard[j][i]);
+        }
       }
-
-      // Check each column
-      let column = [];
-      for (let k = 0; k < currentBingoCard.length; k++) {
-        column.push(currentBingoCard[k][j]);
-      }
-
-      matchingNumbers = column.filter((number) =>
-        numbersCalled.includes(number)
-      );
 
       if (matchingNumbers.length === columnLength) {
-        return currentBingoCard;
+        return bingoCard;
       }
     }
   }
